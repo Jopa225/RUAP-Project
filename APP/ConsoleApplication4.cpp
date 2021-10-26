f43d8fbd518008df65e6f5ac2b0ec064e2772f43
Jopa225 <jopapocrnja22@gmail.com>
2021-10-26 19:11:01 +0200

    bez indexa(obrno)

--- APP/ConsoleApplication4.cpp
+++ APP/ConsoleApplication4.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "„Pozdrav iz aplikacije od Josip Pocrnja! broj indexa 82834823894\n";
+    std::cout << "„Pozdrav iz aplikacije od Josip Pocrnja!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
